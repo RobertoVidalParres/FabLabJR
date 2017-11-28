414ce56d3adef28d50dae988f5195e254b3d8016
Juan Andres <ja.barrachina16@iesdoctorbalmis.com>
2017-11-28 12:22:42 +0100

    Creacion del Recurso cadenaDeConexion para los Gateways y creacion de fichero Gateways Maquina

---
+++ FabLabJR/Gateways/Gateway Maquina.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Gateway_Maquina
+    Dim conexion As SqlConnection
+    Dim comando As New SqlCommand
+
+    Public Sub New(ByRef cadenaConexion As String)
+        conexion = New SqlConnection(My.Settings.cadenaDeConexion)
+        comando = New SqlCommand("", conexion)
+
+        comando.Connection = conexion
+    End Sub
+End Class

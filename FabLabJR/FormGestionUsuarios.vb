0510f6445a7b177c5a1daafa79e0a5f67f8117ec
Juan Andres <ja.barrachina16@iesdoctorbalmis.com>
2017-12-14 12:22:09 +0100

    Creacion Buscador y Busqueda por nombre en  UsuariosDatos y implementacion en FormGestionUsuarios

--- FabLabJR/FormGestionUsuarios.vb
+++ FabLabJR/FormGestionUsuarios.vb
@@ -1,3 +1,13 @@
 ﻿Public Class FormGestionUsuarios
+    'Metodo para que cuando carge el formulario aparezca la tabla rellena con los usuarios de la base de datos
+    Private Sub FormGestionUsuarios_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TablaGestionUsuarioDataGridView.DataSource = UsuariosDatos.ObtenerTablaGestionUsuario()
+    End Sub
+
+    'Metodo para cargar la base de datos con la busqueda por nombre
+    Private Sub BuscarGestionUsuarioButton_Click(sender As Object, e As EventArgs) Handles BuscarGestionUsuarioButton.Click
+        TablaGestionUsuarioDataGridView.DataSource = UsuariosDatos.ObtenerTablaGestionUsuarioPorNombre(BuscadorGestionUsuarioTextBox.Text.Trim())
+    End Sub
+
 
 End Class

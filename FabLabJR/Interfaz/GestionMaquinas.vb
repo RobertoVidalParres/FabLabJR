ad921d2e4b5526833aa83cfb7a0ca8e5d4bdd5ee
Rober <roberto.vidal@iesdoctorbalmis.com>
2017-12-14 12:24:48 +0100

    Diseño gestion maquinas Rober

--- FabLabJR/Interfaz/GestionMaquinas.vb
+++ FabLabJR/Interfaz/GestionMaquinas.vb
@@ -1,3 +1,9 @@
 ﻿Public Class GestionMaquinas
+    Private Sub GestionMaquinas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MaquinasDataGridView.DataSource = GestionarMaquina.ObtenerTablaGestionMaquina
+    End Sub
 
+    Private Sub BuscadorTextbox_TextChanged(sender As Object, e As EventArgs) Handles BuscadorTextbox.TextChanged
+        MaquinasDataGridView.DataSource = GestionarMaquina.ObtenerTablaGestionMaquinaPorModelo("Roland")
+    End Sub
 End Class

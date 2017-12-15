ae87e8d8022b25f3e8c1640e6145dca7b6066c06
Juan Andres <ja.barrachina16@iesdoctorbalmis.com>
2017-12-15 14:20:21 +0100

    Arreglo añadir nueva maquina

--- FabLabJR/My Project/Application.Designer.vb
+++ FabLabJR/My Project/Application.Designer.vb
@@ -34,5 +34,10 @@ Namespace My
         Protected Overrides Sub OnCreateMainForm()
             Me.MainForm = Global.FabLabJR.Form1
         End Sub
+        
+        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
+        Protected Overrides Sub OnCreateSplashScreen()
+            Me.SplashScreen = Global.FabLabJR.Bienvenido
+        End Sub
     End Class
 End Namespace
